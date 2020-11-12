a7d3db2ff133da85dad332fad9524b1aca547125
Sergiy Matusevych <sergiy.matusevych@gmail.com>
2020-11-11 22:10:12 -0800

    bugfix: handle files with special characters in teh file name

--- opus.sh
+++ opus.sh
@@ -6,29 +6,29 @@ OUT_FILE="$2"
 OPUS_HOME="."
 TEMP="${TEMP:-/tmp}"
 
-printf "file\tscore\n" > $OUT_FILE
+printf "file\tscore\n" > "$OUT_FILE"
 
-for IN_WAV in $INPUT_DIR/*.wav
+for IN_WAV in "$INPUT_DIR"/*.wav
 do
     echo "Processing: $IN_WAV"
 
-    BASE="$TEMP/`basename $IN_WAV .wav`"
+    BASE="$TEMP/`basename \"$IN_WAV\" .wav`"
 
     OUT_PCM="$BASE.pcm"
     # OUT_OPUS="$BASE.opus"
     OUT_OPUS=/dev/null
     OUT_LABELS="$BASE.labels"
 
-    sox $IN_WAV -b 16 -c 1 -r 16k -t raw $OUT_PCM > /dev/null
+    sox "$IN_WAV" -b 16 -c 1 -r 16k -t raw "$OUT_PCM" > /dev/null
 
     $OPUS_HOME/opus_demo \
         -e audio 16000 1 8000 -forcemono \
-        $OUT_PCM $OUT_OPUS $OUT_LABELS > /dev/null 2>&1
+        "$OUT_PCM" "$OUT_OPUS" "$OUT_LABELS" > /dev/null 2>&1
 
-    SCORE=`awk '{ sum += $2; ++count } END { printf("%.2f", sum/count) }' < $OUT_LABELS`
+    SCORE=`awk '{ sum += $2; ++count } END { printf("%.2f", sum/count) }' < "$OUT_LABELS"`
 
-    printf "`basename $IN_WAV`\t$SCORE\n" >> $OUT_FILE
+    printf "`basename "$IN_WAV"`\t$SCORE\n" >> "$OUT_FILE"
 
-    rm $OUT_PCM $OUT_LABELS
+    rm "$OUT_PCM" "$OUT_LABELS"
 
 done
