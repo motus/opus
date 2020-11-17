8c25bb2239d3488e045fdc398b988f6f3c688ed4
Sergiy Matusevych <sergiy.matusevych@gmail.com>
2020-11-16 21:24:25 -0800

    update the opus aggregator script to compute the music rating for every 0.1s in each clip

--- opus.sh
+++ opus.sh
@@ -6,7 +6,8 @@ OUT_FILE="$2"
 OPUS_HOME="."
 TEMP="${TEMP:-/tmp}"
 
-printf "file\tscore\n" > "$OUT_FILE"
+# printf "file\tscore\n" > "$OUT_FILE"
+printf "file\tts\tscore\n" > "$OUT_FILE"
 
 for IN_WAV in "$INPUT_DIR"/*.wav
 do
@@ -25,9 +26,26 @@ do
         -e audio 16000 1 8000 -forcemono \
         "$OUT_PCM" "$OUT_OPUS" "$OUT_LABELS" > /dev/null 2>&1
 
-    SCORE=`awk '{ sum += $2; ++count } END { printf("%.2f", sum/count) }' < "$OUT_LABELS"`
+    # ### Compute one aggregate score value per clip
+    # SCORE=`awk '{ sum += $2; ++count } END { printf("%.2f", sum/count) }' < "$OUT_LABELS"`
+    # printf "`basename "$IN_WAV"`\t$SCORE\n" >> "$OUT_FILE"
 
-    printf "`basename "$IN_WAV"`\t$SCORE\n" >> "$OUT_FILE"
+    ### Compute the score for every 0.1s of each clip
+    IN_BASE="`basename \"$IN_WAV\"`" awk '
+
+        $1 % 3200 == 0 {
+            printf("%s\t%.1f\t%.2f\n", ENVIRON["IN_BASE"], $1/32000, sum/count);
+            sum = 0;
+            count = 0;
+        }
+
+        { sum += $2; ++count; }
+
+        END {
+            printf("%s\t%.1f\t%.2f\n", ENVIRON["IN_BASE"], $1/32000, sum/count);
+        }
+
+    ' < "$OUT_LABELS" >> "$OUT_FILE"
 
     rm "$OUT_PCM" "$OUT_LABELS"
 
